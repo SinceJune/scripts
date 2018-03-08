990c0890d403add6370eda27e1a4e9a4cb407627
Chao <SinceJune@users.noreply.github.com>
2018-03-08 15:33:17 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "installing dependency..." && \
 sudo locale-gen zh_CN.UTF-8 && \
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common && \
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
-sudo apt update && sudo apt dist-upgrade && \
+sudo apt update && sudo apt -y dist-upgrade && \
 sudo apt install docker-ce python python-pip default-jdk && \
 sudo sysctl -w vm.max_map_count=262144
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.2.tar.gz && \
